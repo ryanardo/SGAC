5043142855b91511aa9c34055ddf15040138be9a
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-09 14:35:01 -0800

    Add code to convert milliseconds to seconds.

--- app/js/age.calculator.js
+++ app/js/age.calculator.js
@@ -3,3 +3,9 @@ export function ageInMilliseconds(age) {
 	let ageMilliseconds = ageDate.getTime();
 	return ageMilliseconds;
 }
+
+export function milliSecondsToSeconds(ms) {
+	const convertRatio = 0.001;
+	let msToSec = ms * convertRatio;
+	return msToSec;
+}
