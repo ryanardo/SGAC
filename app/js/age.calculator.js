fcf927edc57105b2028b6e501e772981a16b1797
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-09 14:00:17 -0800

    Add code to calculate the difference between two dates, and convert it to seconds.

--- app/js/age.calculator.js
+++ app/js/age.calculator.js
@@ -1,5 +1,22 @@
-export function yearsToSeconds(ageInYears) {
+export function milliSecondsToSeconds(ms) {
+	const convertRatio = 0.001;
+	let msToSec = ms * convertRatio;
+	return msToSec;
+}
+
+export function yearsToSeconds(age1) {
 	const secondsInAYear = 31536000;
-	let ageInSeconds = ageInYears * secondsInAYear
+	let ageInSeconds = age1 * secondsInAYear;
+
 	return ageInSeconds
+}
+
+export function yearDifferenceInSeconds(age1, age2) {
+	let date1 = new Date(age1);
+	let date2 = new Date(age2);
+	let time1 = date1.getTime();
+	let time2 = date2.getTime();
+	let dateDifference = milliSecondsToSeconds(time1) - milliSecondsToSeconds(time2);
+
+	return dateDifference;
 }
