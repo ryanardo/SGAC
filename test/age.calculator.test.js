790d58c5ac2e0c64dda7f8b70066961da52770ba
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-09 14:31:10 -0800

    Test the JS class SGAC.ageInMilliseconds function. PASS.

--- test/age.calculator.test.js
+++ test/age.calculator.test.js
@@ -2,17 +2,15 @@ import * as SGAC from '../app/js/age.calculator.js';
 const assert = require('assert');
 
 describe('SGAC', function () {
+	/* * * * * * * * * * Start: SGAC Class Tests * * * * * * * * * */
+
 	// Calculate seconds in a year
-	describe('yearsToSeconds', function () {
-		it('[yearsToSeconds] should return 31536000 seconds.', function () {
-			assert.equal(31536000, SGAC.yearsToSeconds(1));
-		});
-	});
-	// Can calculate difference in seconds between two dates
-	describe('yearDifferenceInSeconds', function () {
-		it('[yearDifferenceInSeconds] should return 31536000 seconds.', function () {
-			assert.equal(31536000, SGAC.yearDifferenceInSeconds('1971-01-01', '1970-01-01'));
+	describe('ageInMilliseconds', function () {
+		it('[ageInMilliseconds] should return 31536000000 seconds.', function () {
+			assert.equal(31536000000, SGAC.ageInMilliseconds('1971-01-01'));
 		});
 	});
 
-}); //End: SGAC Class Tests
+
+	/* * * * * * * * * * End: SGAC Class Tests * * * * * * * * * */
+});
