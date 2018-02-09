9a952922b33090ec818fec178f74809f08a1dad4
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-09 14:23:28 -0800

    Update age.calculator.js functions.

--- app/js/age.calculator.js
+++ app/js/age.calculator.js
@@ -1,9 +1,21 @@
+export function ageInMilliseconds(age) {
+	let ageDate = new Date(age);
+	let ageMilliseconds = ageDate.getTime();
+	return ageMilliseconds;
+}
+
 export function milliSecondsToSeconds(ms) {
 	const convertRatio = 0.001;
 	let msToSec = ms * convertRatio;
 	return msToSec;
 }
 
+
+
+
+/*
+ORIGINAL FUNCTIONS/CODE
+*/
 export function yearsToSeconds(age1) {
 	const secondsInAYear = 31536000;
 	let ageInSeconds = age1 * secondsInAYear;
@@ -19,4 +31,12 @@ export function yearDifferenceInSeconds(age1, age2) {
 	let dateDifference = milliSecondsToSeconds(time1) - milliSecondsToSeconds(time2);
 
 	return dateDifference;
+}
+
+export function ageCalculation(dob) {
+	let dobDate = new Date(dob);
+	let dobTime = dob.getTime();
+	let currentDate = Date.now();
+
+	let age =
 }
