566298994e0126b552a055e2c6cdba756cec50ac
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-09 16:04:59 -0800

    Add code to calculate age if born on Venus.

--- app/js/age.calculator.js
+++ app/js/age.calculator.js
@@ -16,11 +16,19 @@ export function dateDifference(date1, date2) {
 
 	return milliSecondsToSeconds(d1 - d2);
 }
-
+// Return the age of a human in Mercury years. (A Mercury year is .24 Earth years.)
 export function mercury(age) {
 	const conversionRateMercury = 0.24;
 	let ageEarth = ageInMilliseconds(age);
 	let ageMercury = new Date(ageEarth / conversionRateMercury);
+
+	return Math.abs(ageMercury.getFullYear() - 1970);
+}
+// Return the age of a human in Venus years. (A Venus year is .62 Earth years.)
+export function venus(age) {
+	const conversionRateVenus = 0.62;
+	let ageEarth = ageInMilliseconds(age);
+	let ageMercury = new Date(ageEarth / conversionRateVenus);
 
 	return Math.abs(ageMercury.getFullYear() - 1970);
 }
