cdad3e12f8490516cba36bf8916006d172be312e
Guest User <Guest@Epicodus-3A.local>
2018-02-20 17:48:02 -0800

    SGAC Venus
    Calculate age if on Venus.

--- test/sgac.test.js
+++ test/sgac.test.js
@@ -5,8 +5,22 @@ describe('SGAC', function () {
 	/* * * * * * * * * * Start: SGAC Class Tests * * * * * * * * * */
 	// Calculate age on Earth.
 	describe('ageEarth', function () {
-		it('[ageEarth] shoudl return 28.', function () {
+		it('[ageEarth] should return 28.', function () {
 			assert.equal(28, SGAC.ageEarth('1989-03-07'));
+		});
+	});
+	// Calculate age on Mercury.
+	describe('ageMercury', function () {
+		it('[ageMercury] should return 117.', function () {
+			let age = SGAC.ageEarth('1989-03-07');
+			assert.equal(117, SGAC.ageMercury(age));
+		});
+	});
+	// Calculate age on Venus.
+	describe('ageVenus', function () {
+		it('[ageVenus] should return 45.', function () {
+			let age = SGAC.ageEarth('1989-03-07');
+			assert.equal(45, SGAC.ageVenus(age));
 		});
 	});
 	/* * * * * * * * * * End: SGAC Class Tests * * * * * * * * * */
