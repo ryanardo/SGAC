594b7e07a5d1808aa5338264309ca672fa91232e
Guest User <Guest@Epicodus-3A.local>
2018-02-20 18:06:24 -0800

    Lifespan Overdue
    Test the Lifespan function to make sure it will return a message letting
    you know how many years you have left.

--- test/lifespan.test.js
+++ test/lifespan.test.js
@@ -3,10 +3,16 @@ const assert = require('assert');
 
 describe('Lifespan', function () {
 	/* * * * * * * * * * Start: Lifespan Class Tests * * * * * * * * * */
-	// Calculate age on Earth.
+	// Display warning if Lifespan is overdue.
 	describe('Lifespan overdue', function () {
 		it('[Lifespan] should return a warning.', function () {
 			assert.equal("warning! You are already passed your expected lifespan, be careful!", Lifespan.lifespan(71, 69));
+		});
+	});
+	// Display message if time left on Lifespan.
+	describe('Lifespan OK', function () {
+		it('[Lifespan] should return a message letting you know how many years you have left.', function () {
+			assert.equal("You have 41 years left to go!", Lifespan.lifespan(28, 69));
 		});
 	});
 	/* * * * * * * * * * End: Lifespan Class Tests * * * * * * * * * */
